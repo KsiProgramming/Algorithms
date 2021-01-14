2211593853e3679d9fee6f5b114dbe8bb39e11d6
LaBani Said <76583348+KsiProgramming@users.noreply.github.com>
2021-01-14 22:18:51 +0100

    Add files via upload

---
+++ MergeSort/TestMergingsort.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------# 
+#                   MergeSort Algorithm                      #
+#                                                            #           
+#              (C) 2020, LABANI SAID, France                 #
+#                                                            #
+#               @: labani.said@outlook.com                   #
+# -----------------------------------------------------------#
+
+
+from MergeSort import *
+
+# Test the algorithm for list of numbers
+print('List: ',[1,-2,10,3,5])
+print('After MergeSort Algorithm', MergeSort([1,-2,10,3,5]))
+
+# Test the algorithm for list of letters
+
+print('List: ',['w','z','b','e','s'])
+print('After MergeSort Algorithm',MergeSort(['w','h','b','s','j','e']))
